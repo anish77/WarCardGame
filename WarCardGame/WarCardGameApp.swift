73ba3c2d73b6b5008d0ac440e275c45df820cde9
anish77 <45210460+anish77@users.noreply.github.com>
2022-05-02 10:54:46 +0200

    main

---
+++ WarCardGame/WarCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardGameApp.swift
+//  WarCardGame
+//
+//  Created by Developer on 21/04/22.
+//
+
+import SwiftUI
+
+@main
+struct WarCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
